c07ec07d3939ceaae5ff684bdd7544b182f3c2dc
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-02-25 15:07:48 +0200

    1 and 2 tasks

---
+++ test.js
@@ -0,0 +1,14 @@
+const generator = require('my-lib');
+const q = generator();
+
+function time(iterator, seconds) {
+    const endTime = Date.now() + (seconds * 1000);
+    let lastValue;
+
+    while (Date.now() < endTime) {
+        lastValue = iterator.next().value;
+    }
+
+    console.log(`число операций ${lastValue}`);
+}
+time(q,     0.1);
