5a172a1436d6997fe8236ddff4c573ddf6ab4e85
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-05-17 17:26:20 +0300

    fix: resolve subscribe logic and add console output

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -1,20 +1,22 @@
 class EventEmitter {
     sub = new Map();
+
     subscribe(eventName, callback) {
         if (!this.sub.has(eventName)) {
             this.sub.set(eventName, []);
         }
+
         const arr = this.sub.get(eventName);
-        if(arr){
-            arr.forEach(callback => callback(data));
-        }
+        arr.push(callback);
+
         return () => {
             const index = arr.indexOf(callback);
             if (index !== -1) {
                 arr.splice(index, 1);
             }
         };
-}
+    }
+
     emit(eventName, data) {
         const arr = this.sub.get(eventName);
         if (arr) {
@@ -22,3 +24,22 @@ class EventEmitter {
         }
     }
 }
+
+const mySystem = new EventEmitter();
+
+const unsubscribeLogger = mySystem.subscribe('USER_LOGIN', (user) => {
+    console.log(`Записано: пользователь ${user} вошел в систему.`);
+});
+
+const unsubscribeUI = mySystem.subscribe('USER_LOGIN', (user) => {
+    console.log(`Рисуем плашку: Добро пожаловать, ${user}!`);
+});
+
+console.log('Имитируем первый вход');
+mySystem.emit('USER_LOGIN', 'Kirill');
+
+console.log('\nОтключаем интерфейс');
+unsubscribeUI();
+
+console.log('Имитируем второй вход');
+mySystem.emit('USER_LOGIN', 'Xonex');
