1b442ae7fb938bb9cc8521e27bfd1d126d1d01c0
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-05-19 00:54:21 +0300

    Lab 9: Added async execution and Promise support

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -3,8 +3,20 @@ const fs = require('fs');
 function logDecorator({ level = "INFO", output = "console", format = "text", file = "app.log" }) {
     return function (func) {
         return function (...args) {
+            const startTime = Date.now();
+
+            const writeLog = (result) => {
+                const executionTime = Date.now() - startTime;
+                console.log(`[${new Date().toISOString()}] [${level}] ${func.name}(${args}) -> ${result} (${executionTime}ms)`);
+            };
+
             const result = func(...args);
-            console.log(`[${new Date().toISOString()}] [${level}] ${func.name}(${args}) -> ${result}`);
+
+            if (result instanceof Promise) {
+                return result.then(res => { writeLog(res); return res; });
+            }
+
+            writeLog(result);
             return result;
         };
     };
